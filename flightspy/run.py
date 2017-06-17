e44ddeaf22de7869f5565e168e721a13708b8c27
Pesanteur <pesanteur123@gmail.com>
2017-06-16 21:42:07 -0400

    Adding functionality to track tickets daily

--- flightspy/run.py
+++ flightspy/run.py
@@ -6,6 +6,8 @@ from datetime import datetime
 API_KEY = os.environ.get('API_KEY')
 access = FlightsPy(API_KEY)
 
+from sys import argv
+
 #TODO: Get run.py to run up to 50 days beyond a date and then state the cheapest Ticket Price of the day
 """
 access.grab(params={
@@ -24,11 +26,24 @@ params['origin'] = 'KIN'
 params['destination'] = 'JFK'
 params['solutions'] = 1
 
-today = datetime.today() # If it is too late in the day QPX will return an error, can/maybe should be renamed start_date
-end_date = datetime(2017, 6, 30) # some fixed date in the future TODO: have this be a set interval away from the initial start_date
+today = datetime(2017,6,30) # If it is too late in the day QPX will return an error, can/maybe should be renamed start_date
+end_date = datetime(2017, 7, 30) # some fixed date in the future TODO: have this be a set interval away from the initial start_date
 
-for date in daterange_day(today, 30):
-    date = date.strftime('%Y-%m-%d') # converts date to a form that QPX can recognize
-    params['date'] = date
-    access.grab(params) #TODO: improve this so we won't have to call QPX API each time
-    access.print_result()
+def day_range():
+    for date in daterange_day(today, 30):
+        date = date.strftime('%Y-%m-%d') # converts date to a form that QPX can recognize
+        params['date'] = date
+        access.grab(params) #TODO: improve this so we won't have to call QPX API each time
+        access.print_result()
+
+def set_dates():
+    for date in daterange_day(today, end_date):
+        date = date.strftime('%Y-%m-%d') # converts date to a form that QPX can recognize
+        params['date'] = date
+        access.grab(params) #TODO: improve this so we won't have to call QPX API each time
+
+if __name__ == "__main__":
+    script, instruction = argv
+    if instruction = "dates":
+        pass
+    # TODO: continue building this out
